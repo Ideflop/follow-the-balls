407f823503ae82f8190ba6196051343ac6cf8578
ideflop <vi03pl@gmail.com>
2022-08-18 23:37:09 +0200

    trying to draw circle

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,54 @@
-use glium::glutin;
+use macroquad::prelude::*;
 
-fn main() {
+const BALL_SPEED: f32 = 400f32;
+const BALL_SIZE: f32 = 30f32;
 
-    let mut event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-    event_loop.run(move |ev, _, control_flow| {
-    let next_frame_time = std::time::Instant::now() +
-        std::time::Duration::from_nanos(16_666_667);
-    *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-    match ev {
-        glutin::event::Event::WindowEvent { event, .. } => match event {
-            glutin::event::WindowEvent::CloseRequested => {
-                *control_flow = glutin::event_loop::ControlFlow::Exit;
-                return;
-            },
-            _ => return,
-        },
-        _ => (),
+struct Ball {
+    circle: Circle,
+    vel: Vec2,
+    regular: bool,
+}
+
+impl Ball {
+    pub fn new(pos: Vec3, target: bool) -> Self {
+        Self {
+            circle : Circle::new(pos.x, pos.y, pos.z),
+            vel: vec2(rand::gen_range(-1f32, 1f32), 1f32).normalize(),
+            regular: target,
+        }
     }
-});
+
+    pub fn draw(&self) {
+        let color = match self.regular {
+            true => BLUE,
+            false => GREEN,
+        };
+
+        draw_circle( self.circle.x, self.circle.y, self.circle.r, color);
+    }
+}
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let mut balls = Vec::new();
+    let mut tar = true;
+    
+    for i in 0..10 (
+        if 0<= i && i <= 2 {
+            tar = true;
+        } else {
+            tar = false;
+        },
+
+        balls.push(Ball::new(vec3( screen_width() *( i * 0.1f32), 20f32,15f32), tar))
+    )
+    {     
+    loop {
+        for ball in balls.iter() {
+            ball.draw();
+        }
+
+        next_frame().await
+    }
+    }
 }
