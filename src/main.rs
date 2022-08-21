d3c91f548eeb8c609e346bfec69bebc2dcb28d03
ideflop <vi03pl@gmail.com>
2022-08-21 21:17:45 +0200

    add the start menu

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use macroquad::prelude::*;
+use ::rand::thread_rng;
+use ::rand::seq::index::sample; 
 
 const BALL_SPEED: f32 = 125f32;
 const BALL_SIZE: f32 = 20f32;
+const BALL_SIZE_MENU: f32 = 35f32;
 const BALL_START_POSITION: f32 = 30f32;
+const BALL_START_POSITION_MENU: f32 = 375f32;
+
+pub enum GameState {
+   Menu,
+   Game,
+   Guess,
+   Result,
+}
 
 #[derive(Clone)]
 pub struct Ball {
@@ -20,11 +31,11 @@ impl Ball {
         }
     }
 
-    pub fn draw(&self) {
-        let color = match self.regular {
-            true => BLUE,
-            false => GREEN,
-        };
+    pub fn draw(&self, activ: bool) {
+        let mut color = GREEN;
+        if activ && self.regular {
+            color = BLUE;
+        }
 
         draw_circle( self.circle.x, self.circle.y, self.circle.r, color);
     }
@@ -139,44 +150,131 @@ pub fn collision(a: &mut Ball, b: &Ball){ // change a.circle. to improve ?
     }
 }
 
-pub fn round(x: f32, decimals: u32) -> f32 {
-    let y = 10i32.pow(decimals) as f32;
-    (x * y).round() / y
-}
-
-
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let mut balls = Vec::new();
+    let font = load_ttf_font("font/ubuntu.mono.ttf").await.unwrap();
+    let mut balls= Vec::new();
+    let mut balls_menu = Vec::new();
+    let mut count = 0;
+    let mut game_state = GameState::Menu;
     
-    for i in 0..15 {
-        let tar = i == 0;
-        // let tar = matches!(i, 0..=2); // if 0<=i<=2 then true else false
-        balls.push(Ball::new(vec3( screen_width() *( (i as f32 +0.5) * 0.07f32), BALL_START_POSITION, BALL_SIZE), tar));
+    for i in 0..10 {
+        balls_menu.push(Ball::new(vec3( screen_width() *( (i as f32 +0.5) * 0.1f32), BALL_START_POSITION_MENU, BALL_SIZE_MENU), true));
     }
 
-        
     loop {
         // clear_background(WHITE);
+        match game_state {
+            GameState::Menu => {
+                if is_mouse_button_pressed(MouseButton::Left) {
+                    count = 0;
+                    let cursor_pos = mouse_position();
+                    let mouse_pos = Vec2::new(cursor_pos.0,cursor_pos.1);
+                    for ball in balls_menu.iter() {
+                        count += 1;
+                        if ball.circle.contains(&mouse_pos) {
+                            break;
+                        }
+                    }
+                }
+                
+                for (ct,ball) in balls_menu.iter().enumerate() {
+                    if ct < count {
+                        ball.draw(true);
+                    } else {
+                        ball.draw(false);
+                    }
+                }
 
-        for ball in balls.iter() {
-            ball.draw();
-        }
-        for ball in balls.iter_mut() {
-            ball.update(get_frame_time());
-        }
+                if is_key_down(KeyCode::Space) && count != 0{
+                    game_state = GameState::Game;
 
-        let colli = &balls.clone();
-        
-        for  ball in balls.iter_mut() {
-            for coll in colli.iter() {
-                let one = ball.circle.point();
-                let two = coll.circle.point();
+                    let mut rng = thread_rng(); // rand
+                    let results = sample(&mut rng, 14, count).into_vec(); // rand
 
-                if !(one.x == two.x) && !(one.y == two.y){
-                    collision( ball, coll);
+                    for i in 0..15 {
+                        if results.iter().any(|&l| l==i){
+                            balls.push(Ball::new(vec3( screen_width() *( (i as f32 +0.5) * 0.07f32), BALL_START_POSITION, BALL_SIZE), true));
+                        } else {
+                            balls.push(Ball::new(vec3( screen_width() *( (i as f32 +0.5) * 0.07f32), BALL_START_POSITION, BALL_SIZE), false));
+                        }
+                    }
                 }
             }
+            GameState::Game => {
+                for ball in balls.iter() {
+                    ball.draw(true);
+                }
+                for ball in balls.iter_mut() {
+                    ball.update(get_frame_time());
+                }
+
+                let colli = &balls.clone();
+                
+                for  ball in balls.iter_mut() {
+                    for coll in colli.iter() {
+                        collision( ball, coll);
+                    }
+                }
+            }
+            GameState::Guess => {}
+            GameState::Result => {}
+        }
+
+
+        match game_state {
+            GameState::Menu => {
+                let title  = "Follow the Ball !".to_string();
+                draw_text_ex(&title,
+                             275.0,
+                             100.0,
+                             TextParams { 
+                                 font, 
+                                 font_size: 30u16, 
+                                 color: WHITE, 
+                                 ..Default::default() 
+                            }
+                        );
+
+                let title  = "Choose how many balls you want to find".to_string();
+                draw_text_ex(&title,
+                             175.0,
+                             200.0,
+                             TextParams { 
+                                 font, 
+                                 font_size: 25u16, 
+                                 color: WHITE, 
+                                 ..Default::default() 
+                            }
+                        );
+
+                let title  = "then press SPACE".to_string();
+                draw_text_ex(&title,
+                             300.0,
+                             275.0,
+                             TextParams { 
+                                 font, 
+                                 font_size: 25u16, 
+                                 color: WHITE, 
+                                 ..Default::default() 
+                            }
+                        );
+
+                let title  = &format!("{} balls selected", count);
+                draw_text_ex(title,
+                             300.0,
+                             475.0,
+                             TextParams { 
+                                 font, 
+                                 font_size: 25u16, 
+                                 color: WHITE, 
+                                 ..Default::default() 
+                            }
+                        );
+            }
+            GameState::Game => {}
+            GameState::Guess => {}
+            GameState::Result => {}
         }
         
         next_frame().await
